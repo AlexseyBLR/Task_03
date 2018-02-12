b2b0bee20c902b9d1aac0d502d5afcf344359811
AlexeyMeleschenya <meleschenya1997@gmail.com>
2018-02-13 00:02:58 +0300

    delete catalog

--- ParserImpl/src/main/java/analizator/IAnalizator.java
+++
@@ -1,6 +0,0 @@
-package analizator;
-
-public interface IAnalizator {
-
-    void printAllTags();
-}
--- ProjectWithConnectedLib/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import analizator.FirstAnalizatorImpl;
-
-public class Main {
-    public static void main(String[] args) {
-        FirstAnalizatorImpl analizator = new FirstAnalizatorImpl("E:\\EPAM_Tasks\\Task_03\\ProjectWithLibrary\\src\\main\\resources\\XML.xml");
-        analizator.printAllTags();
-    }
-}
