faa58d215d36b7858de12c55666bd2f3aab7c81c
OSmol <Olga_Smolyakova@outlook.com>
2018-02-20 10:55:19 +0300

    Update FileReader.java

--- SendCatalog/ParserImpl/src/main/java/reader/FileReader.java
+++ SendCatalog/ParserImpl/src/main/java/reader/FileReader.java
@@ -6,10 +6,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+// а закрывать твой сканер будет Пушкин? как можно совершить такую ошибку?
 public class FileReader implements IReader {
 
 
-    private Scanner scanner;
+    private Scanner scanner;// а мы оговаривали, что классом Scanner не пользуемся
 
     public FileReader(String path) throws ReaderException {
         try {
