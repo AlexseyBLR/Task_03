7ad69db0dfba53239f3e4a91198bf144bc52a8f7
OSmol <Olga_Smolyakova@outlook.com>
2018-02-20 10:51:03 +0300

    Update EnumClass.java

--- SendCatalog/ParserImpl/src/main/java/entity/EnumClass.java
+++ SendCatalog/ParserImpl/src/main/java/entity/EnumClass.java
@@ -1,7 +1,8 @@
 package entity;
 
 public class EnumClass {// да чтоб тебя так всю жизнь называли, ну неужели совсем мозни отключаются?
-    public EnumClass() {
+    public EnumClass() { // ты задумался на минутку 
+        // EnumClass.Type.OPEN - вот что это может значить?
     }
 
     public static enum Type {
